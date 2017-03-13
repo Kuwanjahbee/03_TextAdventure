900b1869a9e16ca2047b117ff10cede1e14302e4
ILATIQUA\Spencer <spencerjamesbarrett@gmail.com>
2017-03-13 07:09:00 -0500

    3/13/17 Adding Text Story. Code is going to look like spaghetti.

--- TextAdventure/TextAdventure.cpp
+++ TextAdventure/TextAdventure.cpp
@@ -18,10 +18,13 @@ using namespace std;
 
 void printIntro();
 void playGame();
+
+
 const int totalNumberOfStats = 6;
 int assignedPlayerStats[totalNumberOfStats];
 bool askToPlayAgain();
 string playerName = "";
+int currentRoomDifficulty=0;
 
 
 /*Going to create a class to handle the game logic here, as I'm having trouble getting it into another file.
@@ -59,21 +62,28 @@ void printIntro()
 {
 	cout << "\nHello and welcome to another Text Adventure!\n";
 	cout << "Are you ready for another exciting story?\n\n";
+	cout << "(After a Paragraph, press Enter to Continue)";
+	cin.ignore();
 
 
 	cout << "     You sit patiently in the cockpit of your private vessel," << endl; 
 	cout << "entering in to the Jerusalem Sector. Spending the last six days" << endl; 
 	cout << "in subspace, cut off from communication with the rest of the galaxy" << endl; 
-	cout << "tends to take a toll on some of the less strong willed individuals.'\n" << endl; 
+	cout << "tends to take a toll on some of the less strong willed individuals.'"<< endl;
+	cin.ignore();
+	
+	
 	cout << "     Staring off into the endless black you collect your wits." << endl; 
 	cout << "Pushing on the throttle of your starship and adjusting your pitch" << endl; 
 	cout << "you make for the Astrellas. The captial ship of Jerusalems' " << endl;
-	cout << "'Bounty Hunting Initiative'. Always some business totake care of.\n" << endl;
+	cout << "'Bounty Hunting Initiative'. Always some business totake care of." << endl;
+	cin.ignore();
 
 	cout << "     You begin to near one of the only ships of it's kind. A" <<endl;
 	cout << "dimplomatic ship of destruction that employs dogs of no nation to do" << endl;
 	cout << "it's dirty work for them. The cool gray hull and bright logo of" << endl;
 	cout << "Jerusalem should put one at ease, but not you.\n" << endl;
+	cin.ignore();
 
 	cout << "     'Starship Vega One Niner you are recognized, please forward your" << endl;
 	cout << "name and current health status for our system,' comes a cheery" << endl;
@@ -95,8 +105,6 @@ void playGame()
 	cout << "     'Vega One Niner '" << playerName << "' confirmed, routing health" << endl;
 	cout << "status to your current display. Please confirm?'" << endl<< endl;
 	
-
-	
 	// This is the formula for randomly generating stats, and it scales up from a hardcoded numberofstats value
 	srand(time(0));
 	for (int x = 0; x <= (totalNumberOfStats - 1); x++) { assignedPlayerStats[x] = 3 + (rand() % 7); }
@@ -105,6 +113,7 @@ void playGame()
 	cout << "STR: " << assignedPlayerStats[0] << "     INT: " << assignedPlayerStats[3] << endl;
 	cout << "DEX: " << assignedPlayerStats[1] << "     WIS: " << assignedPlayerStats[4] << endl;
 	cout << "CON: " << assignedPlayerStats[2] << "     CHA: " << assignedPlayerStats[5] << endl << endl;
+	cin.ignore();
 
 	if (assignedPlayerStats[0] > totalNumberOfStats) {
 		cout << "'Damn you must be in good shape!'\n";
@@ -118,19 +127,37 @@ void playGame()
 	cout << endl;
 	cout << "'It says here that you have " << playersRemainingHitPoints <<" health."<< endl;
 	cout << "That seems good enough to me. Go ahead and bring the ship in to dock.'" << endl << endl;
+	cin.ignore();
 
 	// present a challenge to the player
-	cout << "Alright " << playerName << ", I need you to pick up this box for me.\n";
-	if (assignedPlayerStats[0] >= 3) //TODO, make richer return value "roomDifficultyCheck"
+	cout << "    'Alright, " << playerName << ". Lately we've been having trouble with the"<<endl;
+	cout << "auto-docking procedure. Unfortunately you are going to have to bring her" << endl;
+	cout << "in manually. I can't remember if you have been doing this for a while (WIS)" << endl;
+	cout << "or if you are just naturally a smart guy(INT), but go ahead and start the " << endl;
+	cout << "docking process.' " << endl << endl;
+	cin.ignore();
+	if (assignedPlayerStats[3] >= 4 || assignedPlayerStats[4] >=5) //TODO, make richer return value "roomDifficultyCheck"
 	{
-		cout << "*You successfully lift the box!*\n";
+		cout << "      You reach for the console and touch a button that ends the" << endl;
+		cout << "transmission. You've been doing this longer than this greenhorn has" << endl;
+		cout << "been alive. Shifting orientation of your small personal vessel with" << endl;
+		cout << "practiced perfection, like a ballerina in zero G, your ship spins," << endl;
+		cout << "and rolls nearer to the cruiser. You come in faster than you should, " << endl;
+		cout << "pump the breaks and gently nudge against the side of the Astrellas." << endl << endl;
+		
 	}
 	else {
-		cout << "*You strain and hurt yourself*\n";
+		cout << "      You reach for the console and touch a button that ends the" << endl;
+		cout << "transmission. You've been doing this longer than this greenhorn has" << endl;
+		cout << "been alive. Shifting orientation of your small personal vessel with" << endl;
+		cout << "less care than one should. You come in just a touch faster than you" << endl;
+		cout << "anticipate you will. Your personal ship bumps the cruiser with a " << endl;
+		cout << "strong force. A book stored in a container above your head falls on" << endl;
+		cout << "your head, causing a small gash off the side of your forehead." << endl;
 		playersRemainingHitPoints--;
-		cout << playersRemainingHitPoints <<endl <<endl;
+		cout << "(A single point of damage has been done to your health, but the story continues.)" <<endl <<endl;
 	};
-
+	cout << "     The dock process initiatied";
 }
 
 bool askToPlayAgain()
@@ -140,4 +167,6 @@ bool askToPlayAgain()
 	std::getline(std::cin, Response);
 	return (Response[0] =='y' || Response[0] == 'Y');
 }
+
+
 
